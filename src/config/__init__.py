dd934656ee56d8030e765d7baae4160ee363f4ee
Syra <syra_presex@outlook.com>
2025-04-05 10:35:41 -0300

    Delete src directory

--- src/config/__init__.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    def __init__(self, feedback_file: str, min_samples_for_update: int):
-        self.feedback = FeedbackConfig(feedback_file, min_samples_for_update)
-
-class FeedbackConfig:
-    def __init__(self, feedback_file: str, min_samples_for_update: int):
-        self.feedback_file = feedback_file
-        self.min_samples_for_update = min_samples_for_update
